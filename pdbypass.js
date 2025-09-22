c3006da6174b9bb7823844af1d581c75f68d8dd2
tmatzxzone41 <105190138+tmatzxzone41@users.noreply.github.com>
2025-09-23 07:53:12 +0800

    Create pdbypass.js
    :v

---
+++ pdbypass.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name        Bypass pixeldrain download
+// @namespace   Violentmonkey Scripts
+// @match       https://pixeldrain.com/u/*
+// @grant       none
+// @version     1.1
+// @author      TmatzXzonE
+// @description 1/30/2024, 11:47:15 AM
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    // Get the file code from the URL
+    var fileCode = window.location.pathname.split('/').pop();
+
+    // Create and configure the button
+    var newButton = document.createElement('button');
+    newButton.className = 'button svelte-jngqwx';
+    newButton.innerHTML = '<i class="icon">download</i> <span>Bypass Download</span>';
+
+    newButton.addEventListener('click', function() {
+        // Open a new tab with the custom URL
+        window.open('https://pd.cybar.xyz/' + fileCode, '_blank');
+    });
+
+    // Find the existing download button container
+    var downloadContainer = document.querySelector('.description.svelte-jngqwx');
+
+    // Find toolbar element and append the button
+    var toolbar = document.querySelector("div.toolbar.svelte-jngqwx.toolbar_visible");
+    if (toolbar) {
+        toolbar.appendChild(newButton);
+    } else {
+        console.error('Toolbar element not found.');
+    }
+})();
